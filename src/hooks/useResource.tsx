1fd9f113ce19b63c293546e5d4cc432e9f05ac0f
robert nasewytewa <robert.nasewytewa@gmail.com>
2023-10-17 18:52:42 -0700

    fix infinite loop

--- src/hooks/useResource.tsx
+++ src/hooks/useResource.tsx
@@ -4,13 +4,13 @@ import { useEffect, useState } from 'react'
 
 export function useResource (url: string) {
   const [markdown, setMarkdown] = useState(null)
-
+  debugger
   useEffect(() => {
     axios.get(url).then(response => {
       const parsedContent = fm({ content: response.data })
       setMarkdown(parsedContent)
     })
-  })
+  }, [url])
 
   return markdown
 }
